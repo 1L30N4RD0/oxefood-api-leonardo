d3315c69b90209e76514408eac8355f14061d79b
aluno <aluno@ifpe.edu.br>
2024-11-10 18:40:04 -0300

    UPDATE: Produto/Entregador

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+public class ProdutoRepository {
+
+    public Produto save(Produto produto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+    
+}
