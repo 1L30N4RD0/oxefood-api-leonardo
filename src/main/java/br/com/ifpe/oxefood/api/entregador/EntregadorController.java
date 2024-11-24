486919bf108ea80a1d58453a7f3fd0b92073cc57
aluno <aluno@ifpe.edu.br>
2024-11-24 20:13:41 -0300

    integração concluida

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -1,11 +1,10 @@
 package br.com.ifpe.oxefood.api.entregador;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +12,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/api/entregador")
@@ -35,14 +33,9 @@ public class EntregadorController {
     }
 
     @GetMapping
-    public String getMethodName(@RequestParam String param) {
-        return new String();
-    }
-    
     public List<Entregador> listarTodos() {
         return entregadorService.listarTodos();
     }
-
 
     @GetMapping("/{id}")
     public Entregador obterPorID(@PathVariable Long id) {
@@ -55,4 +48,12 @@ public class EntregadorController {
         entregadorService.update(id, request.build());
         return ResponseEntity.ok().build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        entregadorService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
